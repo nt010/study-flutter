646543d0514f6b138f6a08b4f0976d773ac004cf
nt <tomnkmr0120@gmail.com>
2024-03-13 09:23:09 +0900

    API呼び出し

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,67 @@
 import 'package:flutter/material.dart';
-import 'package:myapp/first_page.dart';
-import 'my_home_page.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return MaterialApp( // MaterialAppウィジェットを返すMyAppクラスを定義
+      home: MyHomePage(), // ホーム画面としてMyHomePageを表示
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late Map data; // late修飾子を使用して、後で初期化することを示す
+  late List userData; // late修飾子を使用して、後で初期化することを示す
+
+  Future<void> getData() async {
+    // HTTPリクエストでデータを取得するメソッド
+    http.Response response = await http.get(Uri.parse("https://reqres.in/api/users?page=2")); // Uri.parseで正しいURLを指定
+    data = json.decode(response.body); // レスポンスのデータをMapオブジェクトに変換
+    setState(() {
+      userData = data["data"]; // 必要なデータのみを取り出してuserDataに格納
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData(); // initStateメソッドで初期化処理を行う
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold( // スケルトンとなるマテリアルデザインのウィジェット
+      appBar: AppBar(
+        title: const Text('Flutter Sample'),
+        backgroundColor: Colors.green,
       ),
-      // 最初に見せるページ
-      home: const MyHomePage(title: "yeah!",),
+      body: ListView.builder( // ユーザーデータをリスト表示するListView.builderウィジェット
+        itemCount: userData.length, // リストアイテムの数はuserDataの長さ
+        itemBuilder: (BuildContext context, int index) { // リストアイテムを構築するためのコールバック
+          return Card( // ユーザーデータを表示するカードウィジェット
+            child: Row(
+              children: <Widget>[
+                CircleAvatar( // ユーザープロフィール画像
+                  backgroundImage: NetworkImage(userData[index]["avatar"]), // ユーザープロフィール画像のURLを指定
+                ),
+                Text("${userData[index]["first_name"]} ${userData[index]["last_name"]}"), // ユーザーの名前を表示
+              ],
+            ),
+          );
+        },
+      ),
     );
   }
 }
